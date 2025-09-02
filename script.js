5257e475e78a4ad384cfb4143ea1f263c0d404b2
netinhobabinski <159185041+netinhobabinski@users.noreply.github.com>
2025-09-02 11:20:38 -0300

    02/09

---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+
+const pergunta =[
+ {
+enunciado "pergunta 1",
+alternativas:[
+"alternativas 1",
+"alternativa 2",
+]
+]
+
+}
+
+{
+    enunciado "pergunta 1",
+    alternativas:[
+    "alternativas 1",
+    "alternativa 2",
+    
+
+}
